157516703b007723af93091377c515fed9204d13
SolvedRex <jeep.escobedo@gmail.com>
2023-07-26 01:25:04 -0600

    Added mongodb code impl and cleaned docker

--- backend/src/main/java/com/example/ExampleController.java
+++ backend/src/main/java/com/example/ExampleController.java
@@ -1,13 +1,28 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import com.example.openapi.generated.api.ExampleApi;
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 @Controller
 public class ExampleController implements ExampleApi {
+
+    ClientSession session;
+
+    ExampleController(){
+        MongoClient client = MongoClients.create("mongodb://mongo");
+        MongoDatabase db = client.getDatabase("local");
+        session = client.startSession();
+        try {
+            db.createCollection(session, "credentials");
+            db.createCollection(session, "items");
+        } catch (Exception e) {}
+
+    }
 
     @Override
     public ResponseEntity<String> sayHelloToWorld() {
