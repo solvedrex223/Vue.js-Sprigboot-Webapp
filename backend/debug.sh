e7545a4aa24e66a1a0bec387e964a946f2a1b6b3
emiliano.escobedo <emiliano.escobedo@toshibagcs.com>
2023-07-31 18:32:29 -0600

    Added mongo implementation to query and insert data (incomplete)

---
+++ backend/debug.sh
@@ -0,0 +1,2 @@
+export MAVEN_OPTS='-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n \
+   -jar target/backend-1.0-SNAPSHOT.jar'
--- backend/src/main/java/com/example/ExampleController.java
+++ backend/src/main/java/com/example/ExampleController.java
@@ -1,23 +1,57 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.apache.tomcat.util.json.JSONParser;
+import org.bson.Document;
 import org.springframework.http.ResponseEntity;
 import com.example.openapi.generated.api.*;
+import com.example.openapi.generated.model.ItemInfo;
+import com.mongodb.MongoCommandException;
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @Controller
 public class ExampleController implements StoreApi{
 
-    ClientSession session;
+    private ClientSession session;
+    private MongoDatabase db;
+    private MongoCollection items, credentials;
 
-    ExampleController(){
+    ExampleController() throws Exception{
         MongoClient client = MongoClients.create("mongodb://mongo");
-        MongoDatabase db = client.getDatabase("local");
         session = client.startSession();
-        try {
-            db.createCollection(session, "credentials");
-            db.createCollection(session, "items");
-        } catch (Exception e) {}
+        db = client.getDatabase("local");
+        createDB();
+    }
 
+    private void createDB() throws Exception{
+        try {
+            db.createCollection(session,"credentials");
+            db.createCollection(session,"items");
+            items = db.getCollection("items");
+            credentials = db.getCollection("credentials");
+            JSONParser js = new JSONParser(this.getClass().getResourceAsStream("/BOOT-INF/classes/items.json"));
+            ArrayList<Document> items_documents = new ArrayList<>();
+            for (Object object : js.parseArray()) {
+                items_documents.add(new Document((HashMap<String,?>) object));
+            }
+            items.insertMany(session,items_documents);
+        } catch (Exception e) {
+            if (e.getClass().getName().equals(MongoCommandException.class.getName())) {
+                db.getCollection("credentials").drop();
+                db.getCollection("items").drop();
+                createDB();
+            }
+            else{
+                throw e;
+            }
+        }
     }
 
     @Override
@@ -26,7 +60,8 @@ public class ExampleController implements StoreApi{
     }
 
     @Override
-    public ResponseEntity<String> getItemById(Integer id) {
+    public ResponseEntity<ItemInfo> getItemById(Integer id) {
+        // TODO Auto-generated method stub
         return StoreApi.super.getItemById(id);
     }
 }
