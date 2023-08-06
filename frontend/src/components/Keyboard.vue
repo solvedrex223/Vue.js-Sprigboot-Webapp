d0372b489b32f84b133604e2bfa8ea416ea36c34
Oscar Resendiz <oscar_dino_rey@hotmail.com>
2023-08-05 20:08:59 -0600

    Keyboard

---
+++ frontend/src/components/Keyboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <table>
+    <tr  >
+      <v-btn v-for="tecla in teclas[0]">
+      {{ tecla }}
+      </v-btn>
+    </tr>
+    <tr>
+      <v-btn v-for="tecla in teclas[1]">
+      {{ tecla }}
+      </v-btn>
+    </tr>
+    <tr>
+      <v-btn v-for="tecla in teclas[2]">
+      {{ tecla }}
+      </v-btn>
+    </tr>
+    <tr>
+      <v-btn v-for="tecla in teclas[3]">
+      {{ tecla }}
+      </v-btn>
+    </tr>
+  </table>
+
+  </template>
+  <script setup lang="ts">
+    var teclas:string [][]= [['7','8','9'],['4','5','6'],['1','2','3'],['','0','']]
+
+  </script>
